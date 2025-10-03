372e04a3ffc0f00704bb9eccdaab293c851d6fef
proteet <proteet@node0.736-mini-project.advosuwmadison-pg0.clemson.cloudlab.us>
2025-10-03 13:15:12 +0000

    Add bpf tracing

--- run.sh
+++ run.sh
@@ -1,15 +1,24 @@
 FIO_PATH="${HOME}/fio/fio"
 
-numactl --cpunodebind=0 $FIO_PATH --thread fio_config.ini &
+DURATION=40
+DURATION_PROFILER=$DURATION+5
+
+numactl --cpunodebind=0 $FIO_PATH --thread fio_config.ini --runtime $DURATION &
 FIO_PID=$!
 
-sudo /usr/sbin/profile-bpfcc -F 40000 --pid $FIO_PID -K -f -d 40 --stack-storage-size=40000 > profile_output.txt &
-PROFILE_PID=$!
+numactl --cpunodebind=0 sudo bpftrace -e \
+    "tracepoint:kyber:kyber_adjust { printf(\"Domain: %s, queue depth: %d, device: %d\n\", args->domain, args->depth, args->dev); }" \
+    > bpftrace_output.txt &
+BPFTRACE_PID=$!
+
+sudo numactl --cpunodebind=0 /usr/sbin/biolatency-bpfcc --queued --disk nvme1n1 > biolatency_output.txt $DURATION_PROFILER 1 &
+BIOLATENCY_PID=$!
 
 wait $FIO_PID
 
-echo "Stopping profiler..."
-sudo kill -SIGINT $PROFILE_PID
-wait $PROFILE_PID
+sudo kill -SIGINT $BPFTRACE_PID
+wait $BPFTRACE_PID
 
-FlameGraph/flamegraph.pl --title="Flame Graph for fio" profile_output.txt > flamegraph.svg
+wait $BIOLATENCY_PID
+
+# FlameGraph/flamegraph.pl --title="Flame Graph for fio" profile_output.txt > flamegraph.svg
